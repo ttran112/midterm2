094abf3139b134baa32acc4d642dfff97edf6e8a
ttran112 <ttran112@mail.greenriver.edu>
2021-02-19 17:08:53 -0500

    added index.php composer.json views

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Author Thanh Tran
+ * description: main page for the viewing dating webpage
+ * link: index.php
+ */
+//Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//start session
+session_start();
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+//require_once ('model/data-layer.php');
+
+
+//create an instance of the Base class
+$f3 = Base::instance();
+//set a debug
+$f3 -> set('DEBUG',3);
+
+
+// define a default route
+$f3 -> route('GET|POST /', function ()
+{
+    $view = new Template();
+    echo $view -> render('views/home.html');
+}
+);
+
+//run fat free
+$f3 -> run();
